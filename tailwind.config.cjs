328f6e2a1d6dedd5e4bfe5fa0d0c091f13a57b4a
cniklas <hzc9j_yn@posteo.de>
2022-06-04 23:14:18 +0200

    restore former gray value

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,20 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				tmtGray: {
+					100: '#f7fafc',
+					300: '#e2e8f0',
+					400: '#cbd5e0',
+					500: '#a0aec0',
+					600: '#718096',
+					700: '#4a5568',
+					800: '#2d3748',
+					900: '#1a202c',
+				},
+			},
+		},
 	},
 	plugins: [],
 }
