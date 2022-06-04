6004302421768a0ab0bc8b73e5906d2dc11320da
cniklas <hzc9j_yn@posteo.de>
2022-06-04 10:45:14 +0200

    fix navi active state

--- src/lib/api.js
+++ src/lib/api.js
@@ -15,15 +15,30 @@ const fetchAPI = async (query = 'pages') => {
 	throw new Error(`❗ Failed to fetch API for ${query}\nCode: ${error.code}\nMessage: ${error.message}\n`)
 }
 
-export const getHomePage = async () => {
+export const getPages = async () => {
 	const response = await fetchAPI()
+	const pages = response.map(({ id, modified, slug, link, title, content }) => ({
+		id,
+		isHome: link === WP_HOME,
+		modified,
+		slug,
+		link,
+		title,
+		content,
+	}))
+
+	return pages
+}
+
+export const getHomePage = async () => {
+	const response = await getPages()
 	const page = response.find(item => item.link === WP_HOME)
 
 	return page
 }
 
 export const getAllExceptHomePage = async () => {
-	const response = await fetchAPI()
+	const response = await getPages()
 	const pages = response.filter(item => item.link !== WP_HOME)
 
 	return pages
