6f8077cf6590fbf89d7e9913e93257f3942a8d66
cniklas <hzc9j_yn@posteo.de>
2024-02-14 21:26:58 +0100

    configure prettier

--- src/lib/dateTime.ts
+++ src/lib/dateTime.ts
@@ -1,5 +1,11 @@
 export const formatDate = (date = null) => {
-	const format: Intl.DateTimeFormatOptions = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' }
+	const format: Intl.DateTimeFormatOptions = {
+		year: 'numeric',
+		month: 'long',
+		day: 'numeric',
+		hour: 'numeric',
+		minute: 'numeric',
+	}
 
 	return new Date(date).toLocaleDateString('de-DE', format)
 }
