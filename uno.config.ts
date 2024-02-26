5c2ba42abfc06495554e5bab5cff4c749412c526
Christian Niklas <christian.niklas@outletcity.com>
2024-02-26 08:55:18 +0100

    use `presetMini` instead of `presetWind`

--- uno.config.ts
+++ uno.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, presetWind } from 'unocss'
+import { defineConfig, presetMini } from 'unocss'
 import transformerDirectives from '@unocss/transformer-directives'
 
 export default defineConfig({
@@ -18,8 +18,28 @@ export default defineConfig({
 		},
 	},
 
-	// https://unocss.dev/presets/wind
-	presets: [presetWind({ dark: 'media' })],
+	// custom CSS
+	// missing in presetMini
+	rules: [
+		['uppercase', { 'text-transform': 'uppercase' }],
+		[
+			'sr-only',
+			{
+				position: 'absolute',
+				width: '1px',
+				height: '1px',
+				padding: '0',
+				margin: '-1px',
+				overflow: 'hidden',
+				clip: 'rect(0, 0, 0, 0)',
+				'white-space': 'nowrap',
+				'border-width': '0',
+			},
+		],
+	],
+
+	// https://unocss.dev/presets/mini
+	presets: [presetMini({ dark: 'media' })],
 	// https://unocss.dev/transformers/directives
 	transformers: [transformerDirectives()],
 })
