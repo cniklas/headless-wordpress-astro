d1537a68b5584603cf3362c1783a340bfb0677c6
Christian Niklas <christian.niklas@outletcity.com>
2024-12-06 16:41:34 +0100

    use `light-dark()`, prevent `:hover` styles on touch devices

--- uno.config.ts
+++ uno.config.ts
@@ -2,26 +2,9 @@ import { defineConfig, presetMini } from 'unocss'
 import transformerDirectives from '@unocss/transformer-directives'
 
 export default defineConfig({
-	theme: {
-		colors: {
-			tmtGray: {
-				100: '#f7fafc',
-				300: '#e2e8f0',
-				400: '#cbd5e0',
-				500: '#a0aec0',
-				600: '#718096',
-				650: '#5d6a80',
-				700: '#4a5568',
-				800: '#2d3748',
-				900: '#1a202c',
-			},
-		},
-	},
-
 	// custom CSS
 	// missing in presetMini
 	rules: [
-		['uppercase', { 'text-transform': 'uppercase' }],
 		[
 			'sr-only',
 			{
