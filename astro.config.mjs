eb0b45422467f9525b330b01a77c78721366c152
Christian Niklas <christian.niklas@outletcity.com>
2025-10-01 13:08:20 +0200

    remove obsolete dependency `@unocss/astro`

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,7 @@ import UnoCSS from 'unocss/astro'
 // https://astro.build/config
 export default defineConfig({
 	integrations: [UnoCSS({ injectReset: true })],
+	devToolbar: {
+		enabled: false,
+	},
 })
