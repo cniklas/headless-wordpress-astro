7e676858c2edd08256381c5201ffc83ed100fc68
Christian Niklas <christian.niklas@outletcity.com>
2026-02-19 09:13:45 +0100

    improve link accessibility

--- uno.config.ts
+++ uno.config.ts
@@ -1,6 +1,25 @@
 import { defineConfig, presetMini, transformerDirectives } from 'unocss'
 
 export default defineConfig({
+	// custom CSS
+	// missing in presetMini
+	rules: [
+		[
+			'sr-only',
+			{
+				position: 'absolute',
+				width: '1px',
+				height: '1px',
+				padding: 0,
+				margin: '-1px',
+				overflow: 'hidden',
+				'clip-path': 'inset(50%)',
+				'white-space': 'nowrap',
+				'border-width': 0,
+			},
+		],
+	],
+
 	// https://unocss.dev/presets/mini
 	presets: [presetMini({ dark: 'media' })],
 	// https://unocss.dev/transformers/directives
