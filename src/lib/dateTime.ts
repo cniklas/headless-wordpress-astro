084f36dd163087a8ba0cd9c9718130c46ce0dfd4
Christian Niklas <christian.niklas@outletcity.com>
2026-02-22 08:51:42 +0100

    update date format

--- src/lib/dateTime.ts
+++ src/lib/dateTime.ts
@@ -1,11 +1,11 @@
-export const formatDate = (date: string | number | Date) => {
+export const formatDate = (date: string | number) => {
 	const format: Intl.DateTimeFormatOptions = {
-		year: 'numeric',
+		// year: 'numeric',
 		month: 'long',
 		day: 'numeric',
 		hour: 'numeric',
 		minute: 'numeric',
 	}
 
-	return new Date(date).toLocaleDateString('de', format)
+	return `${new Date(date).toLocaleDateString('de', format)} Uhr`
 }
